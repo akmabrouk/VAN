9a25307ebb8da3ed83569bfa4e3a915cbf26fa12
Han Li <han.li@thomsonreuters.com>
2017-07-14 03:54:58 +0100

    Committing all the files & directories

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+<div align="center">
+<h1>HELLO WORLD</h1>
+
+<?php
+echo "Hello World!";
+?>
+
+<h1><?=$_SERVER['SERVER_ADDR']?></h1>
+<h3><?=$_SERVER['REMOTE_ADDR']?></h3>
+</div>
+</body>
+</html>
